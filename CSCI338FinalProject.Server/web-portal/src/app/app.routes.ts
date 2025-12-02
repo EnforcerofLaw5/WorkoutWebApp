1633e7143f0709ea0700822bcc69d68f8c6bd399
Larry Miller <larry@careevolution.com>
2025-12-01 19:03:41 -0700

    T

--- CSCI338FinalProject.Server/web-portal/src/app/app.routes.ts
+++ CSCI338FinalProject.Server/web-portal/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import { WorkoutsComponent } from './components/workouts/workout.component';
 import { WorkoutDetailComponent } from './components/workouts/workout-detail/workout-detail.component';
+import { WorkoutEditComponent } from './components/workouts/workout-edit/workout-edit.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/workouts', pathMatch: 'full' },
   { path: 'workouts', component: WorkoutsComponent },
-  { path: 'workouts/:id', component: WorkoutDetailComponent },
+  { path: 'workouts/edit/:id', component: WorkoutDetailComponent },
+  { path: 'workouts/new', component: WorkoutEditComponent },
 ];
